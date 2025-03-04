f0c35dba31bec4952d815d444acf14658eb690ae
Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
2025-03-04 15:41:01 +0800

    Almost finished with the backend

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -3,18 +3,42 @@ import ollama
 
 LANGUAGE_MODEL = 'llama3.2:latest'
 
-input_query = input('\nAsk me a question: ')
-while input_query != "/bye":
+def askBot (input_query):
     retrieved_knowledge = retrieve(input_query)
 
     context = "\n\n".join(
-        [f" - {chunk}\nMetadata: {meta}" for chunk, meta in retrieved_knowledge]
+        [f" - {chunk}\nDocument Info: {meta}" for chunk, meta in retrieved_knowledge]
     )
     instruction_prompt = f'''You are a helpful chatbot.
     Use only the following pieces of context to answer the question. When you are answering be reasonable and helpful.
     At the bottom of your answer include which documents you looked at and which pages.
     Don't make up any new information:
-    {retrieved_knowledge}
+    {context}
+    '''
+
+    response = ollama.chat(
+        model=LANGUAGE_MODEL,
+        messages=[
+            {'role': 'system', 'content': instruction_prompt},
+            {'role': 'user', 'content': input_query},
+        ],
+        stream=False,
+    ) 
+
+    return response
+
+input_query = input('\nAsk me a question: ')
+while input_query != "/bye":
+    retrieved_knowledge = retrieve(input_query)
+
+    context = "\n\n".join(
+        [f" - {chunk}\nDocument Info: {meta}" for chunk, meta in retrieved_knowledge]
+    )
+    instruction_prompt = f'''You are a helpful chatbot.
+    Use only the following pieces of context to answer the question. When you are answering be reasonable and helpful.
+    At the bottom of your answer include which documents you looked at and which pages.
+    Don't make up any new information:
+    {context}
     '''
 
     stream = ollama.chat(
@@ -23,12 +47,13 @@ while input_query != "/bye":
             {'role': 'system', 'content': instruction_prompt},
             {'role': 'user', 'content': input_query},
         ],
-        stream=True,
+        stream=False,
     )
 
     print('Chatbot response:')
-    for chunk in stream:
-        print(chunk['message']['content'], end='', flush=True)
+    print(stream['message']['content'])
+    # for chunk in stream:
+    #     print(chunk['message']['content'], end='', flush=True)
     input_query = input('\nAsk me a question: ')
 
 print("Goodbye. Have a great day :D")
